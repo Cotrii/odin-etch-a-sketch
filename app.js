4b9e412cfbad2f98b627e6ee99bcd794c042d406
Cotrii <carlomiguelcortez@gmail.com>
2022-07-26 20:32:56 +0800

    changed color to black

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 let contSqr = document.querySelector(".cont-sqr");
-
 let sizeBtn = document.querySelector(".size");
-
-let homeCont = document.querySelector(".container");
 
 function createDefaultGrid(){
 
@@ -10,14 +7,12 @@ function createDefaultGrid(){
 
     for (let i = 0; i < 16; i++){
         for (let j = 0; j < 16; j++){
-            // console.log("*");
-
             let tempsqr = document.createElement('div');   
     
             tempsqr.setAttribute("class", "sqr");
 
             tempsqr.addEventListener("mouseover", function(event) {
-                event.target.style.backgroundColor = "red";
+                event.target.style.backgroundColor = "black";
             }, false )
             
             contSqr.appendChild(tempsqr);            
@@ -25,13 +20,7 @@ function createDefaultGrid(){
 
     }
 
-    // sizeBtn.addEventListener("click", setSize());
-
 }
-
-let div = contSqr;
-let row = 5;
-let col = 5;
 
 function setSize() {
 
@@ -39,57 +28,35 @@ function setSize() {
         contSqr.removeChild(contSqr.firstChild);
     }
 
-    // homeCont.removeChild(contSqr);
-
     let size = prompt("Enter a number:");
-
-    // // console.log(size);
-
-    // // row = 1;
-    // // col = 1;
-
     newGrid(size);
-
-    // homeCont.appendChild(div);
-
-
 }   
 
 function newGrid(size){
 
-    let temp = "repeat(";
+    let temp = "repeat("; //string temp
     temp += (size + ",1fr)");
 
-    console.log("temp is "+ temp);
 
     if (contSqr instanceof HTMLElement){
         contSqr.style.gridTemplateRows = temp;
         contSqr.style.gridTemplateColumns = temp;
     }
 
-
     for (let i = 0; i < size; i++){
         for (let j = 0; j < size; j++){
-            // console.log("*");
 
             let tempsqr = document.createElement('div');   
     
             tempsqr.setAttribute("class", "sqr");
-
             tempsqr.setAttribute("background-color", "white");
-
             tempsqr.addEventListener("mouseenter", function(event) {
-                
                 event.target.style.backgroundColor = "black";
             }, false )
             
-            div.appendChild(tempsqr);  
-        
+            contSqr.appendChild(tempsqr);  
         }
-
     }
-
-    // sizeBtn.addEventListener("click", setSize());
 
 }
 
